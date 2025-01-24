8b30a969d5e3ff9564f1f7ce326d16097d6b0777
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2025-01-24 20:41:05 -0300

    Classe server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
-import ContaCorrente from "./classes/conta-corrente";
+import { app } from "./app";
 
-const conta = new ContaCorrente(
-  1234,
-  1,
-  "Cezar",
-  "11122233344",
-  new Date("1990-07-12"),
-  new Date("2009-12-31")
-);
-
-try {
-  console.log("Saldo Inicial: ", conta.saldo);
-  conta.setSaldo(100, "C");
-  console.log("Saldo atual", conta.saldo);
-
-  conta.setSaldo(10, "D");
-  console.log("Saldo atual", conta.saldo);
-
-  conta.setSaldo(100, "D");
-  console.log("Saldo atual", conta.saldo);
-} catch (error: any) {
-  console.log(`Erro ao executar operação: ${error.message}`);
-}
-
-console.log(conta);
+(async () => {
+  app.configure();
+  app.start();
+})();
